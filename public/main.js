9310aac6fcd6ebbc4f9526b916e23529f2e46e9b
William Mullen <69743259+William-Mullen@users.noreply.github.com>
2022-04-26 18:55:20 -0400

    start of data sender

--- public/main.js
+++ public/main.js
@@ -1,17 +1,15 @@
 // Focus div based on nav button click
 
 // Flip one coin and show coin image to match result when button clicked
-
 const singlenav = document.getElementById("singlenav")
 // Add event listener for coin button
 singlenav.addEventListener("click", flipCoin)
-			function flipCoin() {
 
-				document.getElementById("single").setAttribute("class", "active")
-				
-                fetch('http://localhost:5000/app/flip/', {mode: 'cors'})
-  				.then(function(response) {
-    			  return response.json();
+	function flipCoin() {
+		document.getElementById("single").setAttribute("class", "active")
+		fetch('http://localhost:5000/app/flip/', {mode: 'cors'})
+		.then(function(response) {
+    		return response.json();
   				})
 				.then(function(result) {
 					console.log(result);
@@ -19,13 +17,24 @@ singlenav.addEventListener("click", flipCoin)
 					document.getElementById("quarter").setAttribute("src", "./assets/img/"+result.flip+".png");
 					//coin.disabled = true
 				})
-//				let flip = "FLIPPED"
-//				document.getElementById("coin").innerHTML = flip;
-//				console.log("Coin has been flipped. Result: "+ flip)
+			//	let flip = "FLIPPED"
+			//	document.getElementById("coin").innerHTML = flip;
+			//	console.log("Coin has been flipped. Result: "+ flip)
 }
-
 
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
-
+const multinav = document.getElementById("multinav")
+//event listner for multinav
+multinav.addEventListener("submit", flipCoins)
+//create the submit handler
+function flipCoins(event){
+	document.getElementById("multi").setAttribute("classs", "active")
+	fetch('http://localhost:5000/app/flip/coins/', {mode: cors})
+}
+//create a data sender
+function sendFlips({url, formData}){
+	const response = fetch()
+	return response.json()
+}
 // Guess a flip by clicking either heads or tails button
